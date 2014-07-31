1c4bd5e124868da6283cdca7bbdade088eb4b5a9
Quis <quiscb@gmail.com>
2014-07-30 22:34:40 -0700

    Athlete List Created Begin onClickListener Athlete List Complete

--- app/src/main/java/com/lbbw/AggiesLand/aggiesland/app/TabBarActivity.java
+++ app/src/main/java/com/lbbw/AggiesLand/aggiesland/app/TabBarActivity.java
@@ -47,6 +47,10 @@ public class TabBarActivity extends TabActivity{
         spec = getTabHost().newTabSpec("Campus Events").setIndicator("Campus Events",res.getDrawable(R.drawable.ic_launcher)).setContent(intent);
         tabHost.addTab(spec);
 
+        intent = new Intent().setClass(this, AthleteListView.class);
+        spec = getTabHost().newTabSpec("Athletics").setIndicator("Athletics",res.getDrawable(R.drawable.ic_launcher)).setContent(intent);
+        tabHost.addTab(spec);
+
 
 
 
